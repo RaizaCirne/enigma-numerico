4652ce8d54e103b53fec7f08b1404d279ea9bbb2
Raiza Cirne <raizacirne@gmail.com>
2024-05-10 09:41:05 -0300

    Feat: Store numbers in array

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,20 @@
+// número do computador VS número do usuário
 // 1º Gerar número randomico e armazenar em uma variável
 // Arredondar número com o Math.floor
-// número do computador VS número do usuário
+
+// 2º Armazenar número do usuário em array
 
 let machineNumber;
+let listNumbers = [];
 
 // Ao carregar a página o computador escolhe um número
 function initialize() {
   machineNumber = Math.floor(Math.random() * 100 + 1);
   console.log(machineNumber);
+}
+
+function compareValues() {
+  const userValue = Number(document.getElementById("inputBox").value);
+  listNumbers.push(" " + userValue);
+  document.getElementById("guesses").innerHTML = listNumbers;
 }
